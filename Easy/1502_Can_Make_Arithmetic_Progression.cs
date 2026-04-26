9b6503b76f5ae00508c8d80899ab2f7fa5734348
Luisander Arroyo Rivera <luisander1830@icloud.com>
2026-04-25 22:16:42 -0400

    Implement CanMakeArithmeticProgression method

---
+++ Easy/1502_Can_Make_Arithmetic_Progression.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public bool CanMakeArithmeticProgression(int[] arr) {
+        Array.Sort(arr);
+        int diff = arr[1] - arr[0];
+        for (int i = 2; i < arr.Length; i++) {
+            if (arr[i] - arr[i - 1] != diff) return false;
+        }
+        return true;
+    }
+}
